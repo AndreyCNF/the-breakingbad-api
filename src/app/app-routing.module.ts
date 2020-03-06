1b84bc57bcd4447bf35f985d1c8689aa13a11410
AndreyCNF <resende.andrey@yahoo.com.br>
2020-03-06 17:23:33 -0300

    Criando os components e adicionando bootstrap ao projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PersonagensDetalhesComponent } from './personagens-detalhes/personagens-detalhes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: {
+            title: 'The Breaking Bad API by Andrey Resende'
+    }
+  },
+  {
+    path: 'personagens/:id',
+    component: PersonagensDetalhesComponent,
+    data: {
+            title: 'Detalhes'
+    }
+  },
+  {
+    path: '',
+    redirectTo: "/home",
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
